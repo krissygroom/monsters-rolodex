46a6b321f8a6b6cde15dc377e432aae51f539d10
krissy <krissydgroom@gmail.com>
2022-02-19 15:55:53 -0800

    Finished app styling

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ class App extends Component {
     );
   }
 
+  handleChange = e => this.setState({ searchField: e.target.value });
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
@@ -22,9 +24,10 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1 className="app_title">Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
